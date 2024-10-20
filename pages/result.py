28b53025d26c32e78f6b60d16a8e94204baad49f
harsh24895 <40701334+harsh24895@users.noreply.github.com>
2024-10-20 16:18:20 -0300

    making webdriver calls

--- pages/result.py
+++ pages/result.py
@@ -2,16 +2,31 @@
 This module contians duckduckGorulesPage
 """
 
+from selenium.webdriver.common.by import By
+
 class DuckduckGoresultPage:
 
+    #locators
+    Result_links = (By.CSS_SELECTOR, 'a.result__a ')
+    SEARCH_INPUT = (By.ID, 'search_form_input_newtab')
+
+    #initizlizer
     def __int__(self, browser):
         self.browser = browser
 
+    #interaction methods
+    # This will return the link titles of results
     def result_link_titles(self):
+        links  = self.browser.find_elements(*self.Result_links)
+        #we are getting the specific text here
+        titles = [link.text for link in links]
+        return titles
 
-        return []
+    #this will get the text value from the search input field from result page
     def search_input_value(self):
-        return ""
+        search_input = self.browser.find_elements(*self.SEARCH_INPUT)
+        value = search_input.get_attribute('value')
+        return value
 
     def title(self):
-        return ""
+        return self.browser.title
--- pages/search.py
+++ pages/search.py
@@ -2,14 +2,28 @@
 module contains DuckduckgoSearchpage,
 the page object for the duckduckgo
 """
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class DuckduckGoSearchPage:
 
+    #url
+    URL = 'https://www.duckduckgo.com'
+
+    #locators
+    SEARCH_INPUT = (By.ID,'search_form_input_newtab')
+
+    #initializer
     def __init__(self, browser):
         self.browser = browser
 
+    #Interaction methods
+    #this will load the webpage
     def load(self):
-        pass
+        self.browser.get(self.URL)
+
 
     def search(self,phrase):
-        pass
+        # * is a standard python thing and passing this we add the tuple which is next to it easily
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
